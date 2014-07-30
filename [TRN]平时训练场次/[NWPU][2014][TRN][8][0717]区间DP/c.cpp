06ae6f5183527446d807b4cb060fd5412ca1aa5c
cfrpg <726525185@qq.com>
2014-07-31 00:30:58 +0800

    本弱也来更新两题orz

---
+++ [TRN]平时训练场次/[NWPU][2014][TRN][8][0717]区间DP/c.cpp
@@ -0,0 +1,36 @@
+﻿//最长回文字串+01滚动
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+char str[5002];
+int n;
+int f[2][5001];
+
+int main()
+{
+	int maxl=0;
+	scanf("%d",&n);
+	scanf("%s",str+1);
+	memset(f,0,sizeof(f));
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			if(str[i]==str[n-j+1])
+					f[i%2][j]=f[(i-1)%2][j-1]+1;
+				else
+					f[i%2][j]=max(f[(i-1)%2][j],f[i%2][j-1]);
+
+			if(maxl<f[i%2][j])
+				maxl=f[i%2][j];
+
+		}
+	}
+
+	printf("%d\n",n-maxl);
+	return 0;
+}
+
---
+++ [TRN]平时训练场次/[NWPU][2014][TRN][8][0717]区间DP/e.cpp
@@ -0,0 +1,56 @@
+﻿//多线程DP
+//f[i][j][k][l]表示两次分别走到(i,j),(k,l)时的最大值
+#include <cstdio>
+#include <cstring>
+
+int map[35][35];
+int f[35][35][35][35];
+int n;
+
+void solve()
+{
+	memset(f,0,sizeof(f));
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			for(int k=0;k<n;k++)
+			{
+				for(int l=0;l<n;l++)
+				{
+
+					f[i][j][k][l]=0;
+					if(i>0&&k>0&&f[i-1][j][k-1][l]>f[i][j][k][l])
+						f[i][j][k][l]=f[i-1][j][k-1][l];
+					if(i>0&&l>0&&f[i-1][j][k][l-1]>f[i][j][k][l])
+						f[i][j][k][l]=f[i-1][j][k][l-1];
+					if(j>0&&k>0&&f[i][j-1][k-1][l]>f[i][j][k][l])
+						f[i][j][k][l]=f[i][j-1][k-1][l];
+					if(i>0&&l>0&&f[i][j-1][k][l-1]>f[i][j][k][l])
+						f[i][j][k][l]=f[i][j-1][k][l-1];
+					if(i==k&&j==l)
+						f[i][j][k][l]+=map[i][j];
+					else
+						f[i][j][k][l]+=map[i][j]+map[k][l];
+				}
+			}
+		}
+	}
+	printf("%d\n",f[n-1][n-1][n-1][n-1]);
+}
+
+int main()
+{
+	while(scanf("%d",&n)!=EOF)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				scanf("%d",&map[i][j]);
+			}
+		}
+		solve();
+	}
+	return 0;
+}
